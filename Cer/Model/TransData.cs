26232a18e3d757207e27eba3aff873225b986e8a
NHBUU <ekkobuu@gmail.com>
2023-06-13 21:57:13 +0700

    sign pdf with created form field 1 times

---
+++ Cer/Model/TransData.cs
@@ -0,0 +1,16 @@
+﻿namespace Cer.Model
+{
+    [Serializable]
+    public class TransData
+    {
+        //public TransData(string progress, int step, string user) 
+        //{
+        //    Progress = progress;
+        //    Step = step;
+        //    User = user;
+        //}
+        public string progress { get; set; }
+        public string step { get; set; }
+        public string user { get; set; }
+    }
+}
